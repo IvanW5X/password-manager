b9449730c2d866441fa155f0fd5720489f943e24
IvanW5X <ivanwong169@gmail.com>
2025-06-14 16:30:38 -0700

    Added functionality to add an account to PM object

--- password-manager.cpp
+++ password-manager.cpp
@@ -9,3 +9,42 @@ PasswordManager::PasswordManager() {
 PasswordManager::PasswordManager(
     std::unordered_map<std::string, Account> services)
     : _services(services) {}
+
+void PasswordManager::addAccount(Account newAccount) {
+  this->_services.insert({newAccount.getService(), newAccount});
+
+  std::cout << "Added new account\n";
+
+  return;
+}
+
+void PasswordManager::getAndAddAccount() {
+  std::string service = "";
+  std::string username = "";
+  std::string password = "";
+
+  std::cout << "Side Note: Do not use space characters in your username or "
+               "password. If entered, spaces will be ignored\n\n"
+            << "Enter Service Name: ";
+  std::getline(std::cin, service);
+
+  std::cout << "Enter Username: ";
+  std::getline(std::cin, username);
+
+  password = getPassword();
+
+  Account newAccount = Account(service, username, password);
+
+  std::cout << "Is this information correct?\n(y/n)\n";
+  newAccount.printAccountInfo();
+
+  std::string confirm = "";
+  std::getline(std::cin, confirm);
+
+  if (confirm == "y" || confirm == "yes")
+    this->addAccount(newAccount);
+  else
+    std::cout << "Account was not added.\n";
+
+  return;
+}
--- password-manager.h
+++ password-manager.h
@@ -1,10 +1,11 @@
 // Header file for PasswordManager class
 
+#include "account.h"
+#include "utils.h"
 #include <iostream>
+#include <random>
 #include <string>
 #include <unordered_map>
-#include <random>
-#include "account.h"
 
 class PasswordManager {
 private:
@@ -13,9 +14,13 @@ private:
 public:
   PasswordManager();
   PasswordManager(std::unordered_map<std::string, Account> services);
+
   void addAccount(Account newAccount);
+  void getAndAddAccount();
   void removeAccount(std::string service);
+  void editAccount(std::string service);
   void printAccounts() const;
+
   std::string encrypt();
   std::string decrypt();
   std::string generatePassword(uint8_t length);
